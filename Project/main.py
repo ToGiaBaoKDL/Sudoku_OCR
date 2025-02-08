71f00e108e16c489888f6e3d9575238fd509f6f2
baodata2226 <136962009+baodata2226@users.noreply.github.com>
2025-02-08 23:24:07 +0700

    Update main.py

--- Project/main.py
+++ Project/main.py
@@ -6,7 +6,7 @@ import time
 
 if __name__ == '__main__':
     start = dt.now()
-    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    # pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
     # Set full-screen layout and page title
     st.set_page_config(page_title="Sudoku Solver", layout="wide")
