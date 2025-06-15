f304d648b3d012a82a25d7bc72e622797f3c9611
baodata2226 <baokdl2226@gmail.com>
2025-06-16 00:00:32 +0700

    Update app

--- app/pages/about.py
+++ app/pages/about.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Add src directory to Python path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 import streamlit as st
 from app.config.settings import PAGE_CONFIG
